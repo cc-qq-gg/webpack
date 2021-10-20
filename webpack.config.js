798223fc27e57663e730bf15d04edd864af578b2
ChenQian <1736014720@qq.com>
2021-10-20 22:30:38 +0800

    配置热更新

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,20 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.[contenthash:8].js',
   },
+  devServer: {
+    port: '3001', // 默认是 8080
+    hot: true,
+    stats: 'errors-only', // 终端仅打印 error
+    compress: true, // 是否启用 gzip 压缩
+    proxy: {
+      '/api': {
+        target: 'http://0.0.0.0:80',
+        pathRewrite: {
+          '/api': '',
+        },
+      },
+    },
+  },
   module: {
     rules: [
       {
